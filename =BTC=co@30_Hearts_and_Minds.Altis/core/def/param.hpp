afbb1ae63c098ec9a95195e2bc7b4735a6a89b2a
Vdauphin <vdauphin@outlook.fr>
2016-06-05 12:24:22 +0200

    FIX: mission parameter allow only integer

--- =BTC=co@30_Hearts_and_Minds.Altis/core/def/param.hpp
+++ =BTC=co@30_Hearts_and_Minds.Altis/core/def/param.hpp
@@ -30,9 +30,9 @@ class Params {
 	class btc_p_ied {
 	//paramsArray[4]
         title = "IEDs ratio:";
-		values[]={0, 0.5, 1, 1.5};
+		values[]={0, 1, 2, 3};
 		texts[]={"Off","Low","Normal","High"};
-		default = 1;
+		default = 2;
 	};
 	class btc_p_engineer {
     //paramsArray[5]
