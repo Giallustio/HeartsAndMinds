ee4c1fccc4ec501d99278d55b157613e78cff384
Vdauphin <vdauphin@outlook.fr>
2016-10-10 21:39:46 +0200

    Add: pacific in param

--- =BTC=co@30_Hearts_and_Minds.Altis/core/def/param.hpp
+++ =BTC=co@30_Hearts_and_Minds.Altis/core/def/param.hpp
@@ -37,8 +37,8 @@ class Params {
 	class btc_p_en {
 	//paramsArray[5]
 		title = "			Enemy type:";
-		values[]={0,1,2,3,4,5,6,7,8,9};
-		texts[]={"Guerrilla (A3)","CAF (@CAF Aggressors)","AAF (A3)","RHS Insurgents (@RHS_AFRF)","Militia (@=BTC= Militia)","Militia (@IWAR: Irregular Warfare)","Syndikat (A3)","Middle East Militia (@Project Opfor)","ISTS (@Project Opfor)","Ultranationalists (@Project Opfor)"};
+		values[]={0,1,2,3,4,5,6,7,8,9,10};
+		texts[]={"Guerrilla (A3)","CAF (@CAF Aggressors)","AAF (A3)","RHS Insurgents (@RHS_AFRF)","Militia (@=BTC= Militia)","Militia (@IWAR: Irregular Warfare)","Syndikat (A3)","Middle East Militia (@Project Opfor)","ISTS (@Project Opfor)","Ultranationalists (@Project Opfor)","CSAT Pacific (A3)"};
 		default = 0;
 	};
 	class btc_p_civ {
@@ -270,6 +270,6 @@ class Params {
 		title = "			Debug:";
 		values[]={0,1,2};
 		texts[]={"No","Yes", "Log only"};
-		default = 0;
+		default = 1;
 	};
 };
