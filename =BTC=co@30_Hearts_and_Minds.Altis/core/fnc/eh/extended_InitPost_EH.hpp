d732987982d20ebbaf287b4cf398f82ecd159904
Vdauphin <vdauphin@outlook.fr>
2017-03-12 11:00:46 +0100

    FIX: error link to https://github.com/Vdauphin/HeartsAndMinds/pull/292/files#diff-6e74a324a7e87b737dffb91b179e2df8

--- =BTC=co@30_Hearts_and_Minds.Altis/core/fnc/eh/extended_InitPost_EH.hpp
+++ =BTC=co@30_Hearts_and_Minds.Altis/core/fnc/eh/extended_InitPost_EH.hpp
@@ -11,7 +11,7 @@ class Extended_InitPost_EventHandlers {
     };
     class Ship {
         class btc_actions {
-            init = "_this call btc_fnc_eh_veh_init";
+            init = "[typeof (_this select 0)] call btc_fnc_eh_veh_init";
         };
     };
 	/*class CAManBase {
