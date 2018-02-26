6e8dad918ee52e2cb3f26f6c376eb856c9c641f2
bux <github@jonathandavid.de>
2018-02-26 14:40:50 +0100

    move xeh declarations to description.ext

---
+++ =BTC=co@30_Hearts_and_Minds.Altis/core/fnc/eh/xeh_InitPost_EH_Vehicle.hpp
@@ -0,0 +1,3 @@
+class btc_actions {
+    init = "[typeof (_this select 0)] call btc_fnc_eh_veh_init";
+};
---
+++ =BTC=co@30_Hearts_and_Minds.Altis/core/fnc/eh/xeh_PreInit_EH.hpp
@@ -0,0 +1,3 @@
+class btc_hearts_and_minds {
+    init = "call compile preprocessFile 'core\fnc\compile.sqf';";
+};
