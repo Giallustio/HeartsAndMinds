db180c53dc734efd4348c559453b22de352abf09
Vdauphin <vdauphin@outlook.fr>
2018-07-03 19:43:59 +0200

    Remove stringtable no more used and dlg
    https://github.com/Vdauphin/HeartsAndMinds/issues/400

--- =BTC=co@30_Hearts_and_Minds.Altis/core/fnc/fob/dlg.hpp
+++ =BTC=co@30_Hearts_and_Minds.Altis/core/fnc/fob/dlg.hpp
@@ -35,56 +35,10 @@ class btc_fob_create {
             h = 0.025 * safezoneH;
         };
         class btc_fob_dlg_apply : btc_dlg_button {
-            text = $STR_BTC_HAM_O_FOB_DLG_T_APPLY; //Apply
+            text = $STR_ui_debug_but_apply; //Apply
             action = "btc_fob_dlg = true;";
             x = 0.45 * safezoneW + safezoneX;
             y = 0.5 * safezoneH + safezoneY;
-            w = 0.1 * safezoneW;
-            default = true;
-        };
-    };
-};
-class btc_fob_redeploy {
-    idd = -1;
-    movingEnable = 1;
-    onLoad = "uiNamespace setVariable [""btc_fob_redeploy"", _this select 0];";
-    objects[] = {};
-    class controlsBackground {
-
-    };
-    class controls {
-        class btc_fob_dlg_re_background : btc_dlg_RscText {
-            idc = -1;
-            x = 0.4 * safezoneW + safezoneX;
-            y = 0.05 * safezoneH + safezoneY;
-            w = 0.2 * safezoneW;
-            h = 0.1 * safezoneH;
-            colorBackground[] = {0, 0, 0, 0.65};
-            text = "";
-        };
-        class btc_fob_dlg_re_text : btc_dlg_RscText {
-            idc = -1;
-            colorBackground[] = {"(profilenamespace getvariable ['GUI_BCG_RGB_R', 0.3843])", "(profilenamespace getvariable ['GUI_BCG_RGB_G', 0.7019])", "(profilenamespace getvariable ['GUI_BCG_RGB_B', 0.8862])", "(profilenamespace getvariable ['GUI_BCG_RGB_A', 0.7])"};
-            x = 0.4 * safezoneW + safezoneX;
-            y = 0.05 * safezoneH + safezoneY;
-            w = 0.2 * safezoneW;
-            h = 0.025 * safezoneH;
-            colorText[] = {1, 1, 1, 1};
-            text = $STR_BTC_HAM_O_FOB_DLG_T_SELECTFOB; //STR_BTC_HAM_O_FOB_DLG_T_SELECTFOB
-        };
-        class btc_fob_dlg_re_fobs: btc_dlg_comboBox {
-            idc = 778;
-            x = 0.4 * safezoneW + safezoneX;
-            y = 0.1 * safezoneH + safezoneY;
-            w = 0.2 * safezoneW;
-            h = 0.017 * safezoneH;
-            onLBSelChanged  = "[] call btc_fnc_fob_lb_change";
-        };
-        class btc_fob_dlg_apply : btc_dlg_button {
-            text = $STR_BTC_HAM_O_FOB_DLG_T_APPLY; //Apply
-            action = "btc_fob_dlg = true;";
-            x = 0.45 * safezoneW + safezoneX;
-            y = 0.125 * safezoneH + safezoneY;
             w = 0.1 * safezoneW;
             default = true;
         };
